fd9ceb9fab1ee4176decf307cc612fb0a4ac3e32
hnegishi <hnegishimaru@gmail.com>
2026-01-17 15:21:59 +0900

    オーディオ設定の保存と読み込み機能を追加し、ミュート状態を管理する機能を強化

--- lib/audio_manager.rb
+++ lib/audio_manager.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+require 'json'
+require 'fileutils'
+
 class AudioManager
   BGM_PATH = 'assets/audio/bgm.mp3'
+  SETTINGS_DIR = 'data'
+  SETTINGS_FILE = "#{SETTINGS_DIR}/audio_settings.json"
 
   class << self
     def play_bgm
       return unless bgm_exists?
       return if @playing
+
+      load_settings if @muted.nil?
 
       @bgm ||= Music.new(BGM_PATH)
       @bgm.loop = true
@@ -41,11 +48,29 @@ class AudioManager
     def toggle_mute
       @muted = !muted?
       @bgm.volume = @muted ? 0 : 100 if @bgm
+      save_settings
       @muted
     end
 
     def muted?
+      load_settings if @muted.nil?
       @muted || false
+    end
+
+    private
+
+    def load_settings
+      return unless File.exist?(SETTINGS_FILE)
+
+      data = JSON.parse(File.read(SETTINGS_FILE))
+      @muted = data['muted'] || false
+    rescue JSON::ParserError
+      @muted = false
+    end
+
+    def save_settings
+      FileUtils.mkdir_p(SETTINGS_DIR)
+      File.write(SETTINGS_FILE, JSON.pretty_generate({ muted: @muted }))
     end
   end
 end
